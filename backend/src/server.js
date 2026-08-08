3a638d4b235d4ad1c0c3d7c69093ef8d2f87637c
Ashutosh Sanjay More <ashutosh.more24@pccoepune.org>
2026-08-08 16:55:32 +0530

    Implement authentication features with login, signup, and user profile routes; add JWT token generation and middleware for protected routes.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 import ENV from "./lib/env.js";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import cors from "cors";
 
 const app = express();
 
 const PORT = ENV.PORT;
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!");
