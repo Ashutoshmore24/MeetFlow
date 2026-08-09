ef23d468a5209b98013a4270cf2447e4ff5f8d14
Ashutosh Sanjay More <ashutosh.more24@pccoepune.org>
2026-08-09 11:38:17 +0530

    Applied Arcjet Protection for rate limiting, spoofed bot detection , SQL injection , etc to all auth routes

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,6 +5,8 @@ const ENV = {
     NODE_ENV: process.env.NODE_ENV,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    ARCJET_KEY: process.env.ARCJET_KEY,
+    ARCJET_ENV: process.env.ARCJET_ENV,
 
 }
 
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { login, signup, logout , userProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection); // Apply Arcjet protection to all routes
 
 router.post("/login", login);
 router.post("/signup", signup);
